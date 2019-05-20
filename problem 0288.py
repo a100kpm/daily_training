959281fb363a8f8f0a14faadca5cd51dee1c1c75
a100kpm <iannis3toussaint@gmail.com>
2019-05-20 16:09:20 +0200

    Create problem 0288.py

---
+++ problem 0288.py
@@ -0,0 +1,43 @@
+'''
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Salesforce.
+
+The number 6174 is known as Kaprekar's contant, after the mathematician who
+ discovered an associated property: for all four-digit numbers with at least 
+ two distinct digits, repeatedly applying a simple procedure eventually results 
+ in this value. The procedure is as follows:
+
+    For a given input x, create two new numbers that consist of the digits in x
+    in ascending and descending order.
+    Subtract the smaller number from the larger number.
+
+For example, this algorithm terminates in three steps when starting from 1234:
+
+    4321 - 1234 = 3087
+    8730 - 0378 = 8352
+    8532 - 2358 = 6174
+
+Write a function that returns how many steps this will take for a given input N.
+'''
+
+num1=4321
+num2=8730
+num3=8532
+
+def kaprekar(num):
+    compteur=0
+    while num!=6174:
+        compteur+=1
+        digit=sorted(",".join(str(num)).split(','))
+        if len(set(digit))<2:
+            print("cann't do")
+            return -compteur
+        num1=int("".join(digit))
+        digit.reverse()
+        num2=int("".join(digit))        
+        num=num2-num1
+
+
+        
+    return compteur
